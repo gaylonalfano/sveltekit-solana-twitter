3eab26c9ca2b626afe2f22c8906e9f345dd91356
gaylonalfano <gaylon.alfano@gmail.com>
2021-12-28 17:33:59 +0800

    Building up some basic components and routes. Some components need to be converted to the corresponding route/index.svelte. Baby steps.

---
+++ app/src/lib/components/PageHome.svelte
@@ -0,0 +1,1 @@
+PageHome
--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,5 +1,19 @@
 <script>
 	import '../app.css';
+	import { base, assets } from '$app/paths';
+	console.log(base);
+	console.log(assets);
+	import Sidebar from '$lib/components/Sidebar.svelte';
 </script>
 
-<slot />
+<div>
+	<div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
+		<Sidebar />
+		<main class="container mx-auto my-6 max-w-lg">
+			<header class="flex space-x-6 items-center justify-between px-8 py-4 border-b">
+				<div class="text-xl font-bold" />
+			</header>
+			<slot />
+		</main>
+	</div>
+</div>
